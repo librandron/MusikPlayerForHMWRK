1f27a23591d918e908624e2888c30371ebcffa59
Андрей М <librandron@gmail.com>
2019-01-31 19:15:25 +0300

    Add project files.

---
+++ Song.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPlayer
+{
+    public class Song : IComparable
+    {
+        public int Duration;
+        public string Name;
+        public Artist Artist;
+        public Album Album;
+
+
+        public int CompareTo(object obj)
+        {
+            return this.Name?.CompareTo((obj as Song).Name) ?? 0;
+            
+        }
+
+        public static string SongCut(this string s)
+        {
+            var cutThis = String.Concat(s);
+            return cutThis;
+        }
+    }
+}
