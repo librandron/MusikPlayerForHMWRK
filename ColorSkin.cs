be76e107dd0602ae4327a4e95e7df91253ca97e1
Андрей М <librandron@gmail.com>
2019-02-01 14:24:43 +0300

    del console.writeline add skin.render in method play()

---
+++ ColorSkin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicPlayer
+{
+    public class ColorSkin: ClassicSkin, ISkin
+    {
+        public ColorSkin(string color) : base()
+        {
+            ConsoleColor option;
+            if (Enum.TryParse(color, out option))
+            {
+                Console.ForegroundColor = option;
+            }
+            else Console.WriteLine($"Couldn't set {option} color");
+        }
+    }
+}
